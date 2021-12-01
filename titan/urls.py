dfd12edde9ab7782e08c782cf6758895c4006ca7
mealilo <90289295+mealilo@users.noreply.github.com>
2021-12-01 13:40:27 -0700

    routes set up and templates and views

--- titan/urls.py
+++ titan/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
-from .views import  indexPageView 
+from .views import  indexPageView, aboutPageView, searchPageView, detailsPageView
 urlpatterns = [
+    path("about/", aboutPageView, name="about"),
+    path("search/", searchPageView, name="search"),
+    path("detail/", detailsPageView, name="detail"),
     path("", indexPageView, name="index"),
         ]
--- titan/views.py
+++ titan/views.py
@@ -2,4 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 def indexPageView(request) :
-    return render(request, 'titan/index.html') 
+    return render(request, 'titan/index.html')
+
+def aboutPageView(request) :
+    return render(request, 'titan/about.html') 
+
+def searchPageView(request) :
+    return render(request, 'titan/search.html')
+def detailsPageView(request) :
+    return render(request, 'titan/details.html') 
