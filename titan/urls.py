bc50daa2acbbdbbaf1c37976b66fc9b9fee7e11d
mealilo <90289295+mealilo@users.noreply.github.com>
2021-12-02 15:03:07 -0700

    more pages. routing

--- titan/urls.py
+++ titan/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import  indexPageView, aboutPageView, searchPageView, detailsPageView, statisticsPageView
+from .views import  indexPageView, aboutPageView, searchPageView, detailsPageView, statisticsPageView, detailsdrugsPageView
 urlpatterns = [
     path("about/", aboutPageView, name="about"),
     path("search/", searchPageView, name="search"),
-    path("details/", detailsPageView, name="details"),
+    path("details/<int:prescriberid>", detailsPageView, name="details"),
+    path("details/<int:drugid>", detailsdrugsPageView, name="detailsdrugs"),
     path("statistics/", statisticsPageView, name="statistics"),
     path("", indexPageView, name="index"),
         ]
--- titan/views.py
+++ titan/views.py
@@ -8,9 +8,22 @@ def aboutPageView(request) :
     return render(request, 'titan/about.html') 
 
 def searchPageView(request) :
-    return render(request, 'titan/search.html')
-def detailsPageView(request) :
+    data = ['drug1','drug2','drug3']
+    context = {
+        'resultset' : data,
+    }
+    return render(request, 'titan/search.html', context)
+def detailsPageView(request, prescriberid ) :
     return render(request, 'titan/details.html')
+def detailsdrugsPageView(request, drugid) :
 
+    # data = Student.objects.all()
+    # loading = Grade_Level.objects.all()
+
+    data = ['drug1','drug2','drug3']
+    context = {
+        'resultset' : data,
+    }
+    return render(request, 'titan/detailsdrugs.html', context)
 def statisticsPageView(request) :
     return render(request, 'titan/statistics.html') 
