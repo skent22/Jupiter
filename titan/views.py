16e21662c55589fc4998015aeae78bcd7120f471
mealilo <90289295+mealilo@users.noreply.github.com>
2021-12-02 18:01:28 -0700

    matt is a chod

--- titan/views.py
+++ titan/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import redirect, render
+from django import forms
 
 # Create your views here.
 def indexPageView(request) :
@@ -8,13 +9,29 @@ def aboutPageView(request) :
     return render(request, 'titan/about.html') 
 
 def searchPageView(request) :
+
+    if request.method == 'GET':
+        test = 'frick spencer'
+        name = request.GET
+        if 'prescriberform' in name.keys():
+            test ='frick me' 
+            # firstname = request.GET['firstname']
+            # firstname = request.GET['firstname']
+            # firstname = request.GET['firstname']
+            # firstname = request.GET['firstname']
+            #data = Employee.objects.filter(emp_first=sFirst, emp_last=sLast)
+
     data = ['drug1','drug2','drug3']
     context = {
         'resultset' : data,
+        'test': test,
+        'name':name
     }
     return render(request, 'titan/search.html', context)
+
 def detailsPageView(request, prescriberid ) :
     return render(request, 'titan/details.html')
+
 def detailsdrugsPageView(request, drugid) :
 
     #get drug object based on drugid
@@ -30,10 +47,3 @@ def statisticsPageView(request) :
 def addprescriberPageView(request) :
     return render(request, 'titan/addprescriber.html') 
 
-
-
-
-#Logic
-
-def searchPrescriber(request):
-    return redirect('searchPageView')
