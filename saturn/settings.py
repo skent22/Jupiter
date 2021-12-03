5229fee045849a7068d1e6a621b10ba13bfe5687
shar4901 <90164014+shar4901@users.noreply.github.com>
2021-12-03 13:33:42 -0700

    updated models

--- saturn/settings.py
+++ saturn/settings.py
@@ -74,16 +74,16 @@ WSGI_APPLICATION = 'saturn.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#     'ENGINE': 'django.db.backends.postgresql',
-#     'NAME': 'intex',
-#     'USER': 'postgres',
-#     'PASSWORD': 'mcorbet3',
-#     'HOST': 'localhost',
-#     'DISABLE_SERVER_SIDE_CURSORS': True
-#     }
-# } 
+DATABASES = {
+    'default': {
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': 'intexscript',
+    'USER': 'postgres',
+    'PASSWORD': 'Happ1n3ss',
+    'HOST': 'localhost',
+    'DISABLE_SERVER_SIDE_CURSORS': True
+    }
+} 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
--- titan/models.py
+++ titan/models.py
@@ -6,8 +6,8 @@ from django.db.models.fields import CharField
 
 # Create your models here.
 class drug(models.Model):
-    drugid = models.IntegerField(unique = True, null=False,primary_key=True, editable=False,default='')
-    drugname = models.CharField(verbose_name='Drug Name',max_length=30, unique=True, null=False,default='')
+    drugid = models.IntegerField(unique = True, null=False, editable=False,default='')
+    drugname = models.CharField(verbose_name='Drug Name',max_length=30, unique=True, null=False,default='',primary_key=True)
     isopioid = models.BooleanField(null=False,verbose_name='Is Opioid')
 
     def __str__ (self):
@@ -44,14 +44,16 @@ class state(models.Model):
         verbose_name = 'State'
 
 class triple(models.Model):
-    npi = models.ForeignKey(prescriber, verbose_name=("Prescriber"), on_delete=models.CASCADE, null=False,primary_key=True,default='')
-    drugname = models.ForeignKey(drug, verbose_name=("DrugName"), on_delete=models.CASCADE, null=False,default='')
+    prescriberid = models.ForeignKey(prescriber,db_column='prescriberid', verbose_name=("Prescriber"), on_delete=models.CASCADE, null=False,primary_key=True,default='')
+    drugname = models.ForeignKey(drug,db_column='drugname', verbose_name=("DrugName"), on_delete=models.CASCADE, null=False,default='')
     qty = models.IntegerField(null=False)
 
+    def __str__ (self):
+        return (str(self.prescriberid) + ' - ' + str(self.drugname) + ' - ' + str(self.qty))
     class Meta:
         db_table = 'pd_triple'
         verbose_name = 'Triple'
-        unique_together = (('npi', 'drugname'))
+        unique_together = (('prescriberid', 'drugname'))
 
 class credential(models.Model):
     cred_id = models.IntegerField(unique = True, null=False,primary_key=True,editable=False,default='')
@@ -63,10 +65,11 @@ class credential(models.Model):
         verbose_name = 'Credential'
 
 class link(models.Model):
-    npi = models.ForeignKey(prescriber, verbose_name=("NPI"), on_delete=models.CASCADE, null=False,primary_key=True,default='')
-    cred_id = models.ForeignKey(credential, verbose_name=("Credential_id"), on_delete=models.CASCADE, null=False,default='')
+    npi = models.ForeignKey(prescriber, db_column= 'npi',verbose_name=("NPI"), on_delete=models.CASCADE, null=False,primary_key=True,default='')
+    cred_id = models.ForeignKey(credential,db_column='cred_id', verbose_name=("Credential_id"), on_delete=models.CASCADE, null=False,default='')
 
-
+    def __str__ (self):
+        return (str(self.npi) + ' - ' + str(self.cred_id))
     class Meta:
         db_table = 'linking'
         verbose_name = 'Prescriber_Credential'
