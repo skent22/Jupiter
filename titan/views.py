1be66c7569d2655ee091ac8eac2b0db182bc042e
mealilo <90289295+mealilo@users.noreply.github.com>
2021-12-03 18:09:45 -0700

    working drop downs!

--- titan/views.py
+++ titan/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import redirect, render
 from django import forms
 
-from titan.models import drug, prescriber
+from titan.models import drug, prescriber, state, credential
 from django.db.models import Q
 # Create your views here.
 def indexPageView(request) :
@@ -11,16 +11,15 @@ def aboutPageView(request) :
     return render(request, 'titan/about.html') 
 
 def searchPageView(request) :
-    newlist = []
     data = ''
     sql = ''
     form = ""
+
+    states = state.objects.all()
+    credentials = credential.objects.all()
     if request.method == 'GET':
-        test = 'frick spencer'
-        
         name = request.GET
         if 'prescriberform' in name.keys():
-            test = 'frick me'
             form = 'prescriberform'
             params = {
             'firstname' : request.GET['firstname'].title(),
@@ -30,13 +29,6 @@ def searchPageView(request) :
             'gender' : request.GET['gender']}
             if request.GET['gender'] != "":
                 gender = request.GET['gender']
-            # newlist = [firstname, lastname, state, credential, gender]
-            # data = Prescriber.objects.filter(fname__contains = firstname,
-            #  lname__contains = lastname,
-            #  state__contains = state,
-            #  credential__contains = credential,
-            #  gender__contains = gender
-            #   )
             sql = 'SELECT * FROM pd_prescriber inner join linking on linking.npi = pd_prescriber.npi inner join credentials on credentials.cred_id = linking.cred_id WHERE 1=1'
            
 
@@ -46,16 +38,13 @@ def searchPageView(request) :
                 sql += ' AND (fname LIKE ' +  '\''+str(params['lastname'])+'\'' + 'or  lname like ' + '\'' + str(params['lastname'] + '\')')
             if params['state'] != '':
                 sql += ' AND (state LIKE ' +  '\''+str(params['state'])+'\') '
-            if params['state'] != '':
-                sql += ' AND (state LIKE ' +  '\''+str(params['state'])+'\') '
             if params['credential'] != '':
                 sql += ' AND (abbreviation = ' +  '\''+str(params['credential'])+'\') '
             if params['gender'] != '':
                 sql += ' AND (gender = ' +  '\''+str(params['gender'])+'\') '
             sql += ' order by lname'
             data = prescriber.objects.raw(sql)
-            # data = prescriber.objects.filter((Q(fname__contains=params['firstname']) | Q(lname__contains=params['firstname']))) 
-        
+
         elif 'drugform' in name.keys():
             form = 'drugform'
             medicationname = request.GET['medicationname']
@@ -68,11 +57,12 @@ def searchPageView(request) :
                 sql += ' AND isopioid = ' + '\'' + isopioid + '\''
             sql += ' order by drugname'
             data = drug.objects.raw(sql)
-    # data = ['drug1','drug2','drug3']
     context = {
         'resultset' : data,
         'test': sql,
-        'form': form
+        'form': form,
+        'states': states,
+        'credentials': credentials,
     }
     return render(request, 'titan/search.html', context)
 
@@ -80,10 +70,7 @@ def detailsPageView(request, prescriberid ) :
     return render(request, 'titan/details.html')
 
 def detailsdrugsPageView(request, drugid) :
-    
-    #get drug object based on drugid
-
-    data = ['drug1','drug2','drug3']
+    data = []
     context = {
         'resultset' : data,
     }
